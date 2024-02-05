c51a23c3e3a6b0a683ee44c3cca68f863fb9bc4a
Aleksan_09 <platondovgan2006@gmail.com>
2024-02-05 18:32:31 +0300

    Часть №3

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import sys
 import pygame
 import requests
 
+height = 20.284978
+width = -25.348019
 params = {
-    'll': '20.284978,-25.348019',
+    'll': ','.join([str(height), str(width)]),
     'z': 2,
     'l': 'sat'
 }
@@ -39,7 +41,7 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_PAGEUP:
                 new_params = {
-                    'll': '20.284978,-25.348019',
+                    'll': ','.join([str(height), str(width)]),
                     'z': params['z'] + 1 if params['z'] < 21 else params['z'],
                     'l': 'sat'
                 }
@@ -57,8 +59,85 @@ while running:
                     pygame.display.flip()
             if event.key == pygame.K_PAGEDOWN:
                 new_params = {
-                    'll': '20.284978,-25.348019',
+                    'll': ','.join([str(height), str(width)]),
                     'z': params['z'] - 1 if params['z'] > 0 else params['z'],
+                    'l': 'sat'
+                }
+
+                map_request = "https://static-maps.yandex.ru/1.x/"
+                response = requests.get(url=map_request, params=new_params)
+
+                if response.status_code == 200:
+                    params = new_params
+                    map_file = "map.png"
+                    with open(map_file, "wb") as file:
+                        file.write(response.content)
+
+                    screen.blit(pygame.image.load(map_file), (0, 0))
+                    pygame.display.flip()
+            if event.key == pygame.K_LEFT:
+                height = height - 10 if height > -180 else height
+                new_params = {
+                    'll': ','.join([str(height), str(width)]),
+                    'z': params['z'],
+                    'l': 'sat'
+                }
+
+                map_request = "https://static-maps.yandex.ru/1.x/"
+                response = requests.get(url=map_request, params=new_params)
+
+                if response.status_code == 200:
+                    params = new_params
+                    map_file = "map.png"
+                    with open(map_file, "wb") as file:
+                        file.write(response.content)
+
+                    screen.blit(pygame.image.load(map_file), (0, 0))
+                    pygame.display.flip()
+            if event.key == pygame.K_RIGHT:
+                height = height + 10 if height < 180 else height
+                new_params = {
+                    'll': ','.join([str(height), str(width)]),
+                    'z': params['z'],
+                    'l': 'sat'
+                }
+
+                map_request = "https://static-maps.yandex.ru/1.x/"
+                response = requests.get(url=map_request, params=new_params)
+
+                if response.status_code == 200:
+                    params = new_params
+                    map_file = "map.png"
+                    with open(map_file, "wb") as file:
+                        file.write(response.content)
+
+                    screen.blit(pygame.image.load(map_file), (0, 0))
+                    pygame.display.flip()
+
+            if event.key == pygame.K_UP:
+                width = width + 10 if width < 90 else width
+                new_params = {
+                    'll': ','.join([str(height), str(width)]),
+                    'z': params['z'],
+                    'l': 'sat'
+                }
+
+                map_request = "https://static-maps.yandex.ru/1.x/"
+                response = requests.get(url=map_request, params=new_params)
+
+                if response.status_code == 200:
+                    params = new_params
+                    map_file = "map.png"
+                    with open(map_file, "wb") as file:
+                        file.write(response.content)
+
+                    screen.blit(pygame.image.load(map_file), (0, 0))
+                    pygame.display.flip()
+            if event.key == pygame.K_DOWN:
+                width = width - 10 if width > -90 else width
+                new_params = {
+                    'll': ','.join([str(height), str(width)]),
+                    'z': params['z'],
                     'l': 'sat'
                 }
 
