fbfb0dc664a9effb2ec0073024237330a0bdab46
Aleksan_09 <platondovgan2006@gmail.com>
2024-02-05 18:13:21 +0300

    Часть №2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 
 params = {
     'll': '20.284978,-25.348019',
-    'z': '2',
+    'z': 2,
     'l': 'sat'
 }
 
@@ -36,6 +36,43 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEUP:
+                new_params = {
+                    'll': '20.284978,-25.348019',
+                    'z': params['z'] + 1 if params['z'] < 21 else params['z'],
+                    'l': 'sat'
+                }
+
+                map_request = "https://static-maps.yandex.ru/1.x/"
+                response = requests.get(url=map_request, params=new_params)
+
+                if response.status_code == 200:
+                    params = new_params
+                    map_file = "map.png"
+                    with open(map_file, "wb") as file:
+                        file.write(response.content)
+
+                    screen.blit(pygame.image.load(map_file), (0, 0))
+                    pygame.display.flip()
+            if event.key == pygame.K_PAGEDOWN:
+                new_params = {
+                    'll': '20.284978,-25.348019',
+                    'z': params['z'] - 1 if params['z'] > 0 else params['z'],
+                    'l': 'sat'
+                }
+
+                map_request = "https://static-maps.yandex.ru/1.x/"
+                response = requests.get(url=map_request, params=new_params)
+
+                if response.status_code == 200:
+                    params = new_params
+                    map_file = "map.png"
+                    with open(map_file, "wb") as file:
+                        file.write(response.content)
+
+                    screen.blit(pygame.image.load(map_file), (0, 0))
+                    pygame.display.flip()
 pygame.quit()
 
 # Удаляем за собой файл с изображением.
